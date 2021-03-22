4e0c345e05ad16066943b85c33653a80688dc9d5
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-22 16:26:18 +0300

    First Commit

---
+++ 8.C sharp Ife Else with Checkboxes/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8.C_sharp_Ife_Else_with_Checkboxes
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			//Checked is a property of a box, this property is either true or false
+			//true or false values are used with if/else blocks to make decisions
+			if (checkBox1.Checked)
+			{
+				MessageBox.Show("Are you sure you want to get rich?");//runs when the box is checked
+			}
+			else
+			{
+				MessageBox.Show("Are you sure you don't want to get rich?");//runs when the box is not checked
+			}
+		}
+	}
+}
